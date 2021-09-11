012ae597a60b75b73718ef594a3d9a36d2ca3ef5
Hayoung <gkduddy0814@gmail.com>
2021-09-11 13:08:02 +0900

    feat : add profiles pages

---
+++ pages/[profiles].tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '../components/ui/Container';
+
+const UserProfile = () => {
+  return (
+    <Container>
+      <h1>프로필페이지</h1>
+    </Container>
+  );
+};
+
+export default UserProfile;
