acd52b61f84172273ab898df7f84539ab3b49ad3
Hayoung <gkduddy0814@gmail.com>
2021-09-11 13:04:05 +0900

    feat : first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { Provider } from 'react-redux';
+import Layout from '../components/common/Layout';
+import { store } from '../lib/redux/store';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </>
+  );
 }
-export default MyApp
