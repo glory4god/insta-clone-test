9653d196e39d78c0e99c22d466bf53684c114a6e
Hayoung <gkduddy0814@gmail.com>
2021-09-11 13:05:37 +0900

    feat : add Navbar on fixed top banner

---
+++ components/common/Navbar/Navbar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Paper from '@material-ui/core/Paper';
+import s from './Navbar.module.css';
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className={s.paper}>
+        <div className={s.navigator}>
+          <div>이미지</div>
+          <div>검색</div>
+          <div>네비</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
